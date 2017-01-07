7ac39a0dad92d9362cb960b7aeae00b54b570e73
fa2k <pmb@fa2k.net>
2017-01-07 21:33:21 +0100

    Build scripts for duplicate-finder (not very pretty)

---
+++ subrbuild.sh
@@ -0,0 +1,2 @@
+scl enable devtoolset-3 "g++ -fopenmp -std=c++11 -O3 -o duplicate-finder.subrange duplicate-finder.subrange.cpp -lz /usr/lib64/libboost_iostreams.a"
+
---
+++ ubuntu-build.sh
@@ -0,0 +1,1 @@
+g++ -fopenmp -std=c++11 -O3 -o duplicate-finder.subrange duplicate-finder.subrange.cpp -lz /usr/lib/x86_64-linux-gnu/libboost_iostreams.so
