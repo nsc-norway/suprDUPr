423ba6c826ded50e018871d49becc996dbde2b3f
fa2k <pmb@fa2k.net>
2017-06-12 20:53:27 +0200

    Compile only the main program by default

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-g++ -fopenmp -std=c++11 -O3 -o duplicate-finder.subrange duplicate-finder.subrange.cpp -lz /usr/lib/x86_64-linux-gnu/libboost_iostreams.so
+#g++ -fopenmp -std=c++11 -O3 -o duplicate-finder.subrange duplicate-finder.subrange.cpp -lz /usr/lib/x86_64-linux-gnu/libboost_iostreams.so
 g++ -std=c++11 -O3 -o fastdup fastdup.cpp -lz -lboost_program_options -lboost_iostreams
