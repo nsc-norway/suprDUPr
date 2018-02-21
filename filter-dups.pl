a2e6a1e9cdf5c13446819f56cb10621fdccebce5
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2018-02-21 22:30:24 +0100

    Attempted to make a filtering tool

--- filter-dups.pl
+++ filter-dups.pl
@@ -1,30 +1,54 @@
-#if [ "$1" == "-h" ]
-#then
-#    echo " -- FASTQ Filter script --"
-#    echo ""
-#    echo " This script may be used in conjunction with fastdup.read_id"
-#    echo " to filter FASTQ files for local duplicates."
-#    echo " To output a FASTQ file with duplicates removed to standard"
-#    echo " output, use:"
-#    echo ""
-#    echo " $ ./fastdup.read_id INPUT_FILE | ./filter-dups.sh INPUT_FILE"
-#    echo ""
-#    echo " (note that the input file has to be specified twice)"
-#    
-#fi
-open(my $fastq_file, $1);
-# TODO: this can probably be written better
-open(my $stdin, '-');
-while(<$stdin>) {
-    $_ =~ s/ .*//; # ugh
-    my $fqheader = '@' . $_;
-    my @lines =  TODO.... 
+use IO::Uncompress::Gunzip qw(gunzip $GunzipError) ;
+my $fastq_file_path = shift;
+
+if (!$fastq_file_path) {
+    print(<<END
+    
+    == FASTQ Filter script ==
+
+  This script may be used in conjunction with fastdup.read_id
+  to filter FASTQ files for local duplicates.
+  To output a FASTQ file with duplicates removed to standard
+  output, use:
+ 
+  \$ ./fastdup.read_id INPUT_FILE | ./filter-dups.sh INPUT_FILE
+
+  (note that the input file has to be specified twice)
+
+ Out of any group of duplicate reads, the read occurring at the 
+ first position in the file is chosen as the 'original', and is 
+ written to the output. The subsequent duplicates are discarded.
+
+END
+);
+ die('Missing required argument INPUT_FILE.');
+}
+
+my $fastq_file = new IO::Uncompress::Gunzip($fastq_file_path, "transparent", 1);
+open(my $fastdup_input, '-');
+while(<$fastq_file>) {
+    print;
+}
+exit(0);
+
+while (<$fastdup_input>) {
+    my @parts = split;
+    my $fqheader = '@' . @parts[0];
+    my $found = 0;
     while(<$fastq_file>) {
-        if TODO field 1 == $fqheader {
-            # discard 3 more lines
-            break;
+        @fqparts = split;
+        if ($found) {
+            print "C1\n";
+            if (++$found == 3) {
+                last;
+            }
+        }
+        elsif (@fqparts[0] eq $fqheader) {
+            print "C2\n";
+            $found = 1;
         }
         else {
+            print "C3\n";
             print;
         }
     }
